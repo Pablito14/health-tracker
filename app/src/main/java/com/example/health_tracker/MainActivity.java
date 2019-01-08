3b4bdc364f475077c4cf27333ea3efb490322601
pablo <pablo.rosales14@gmail.com>
2019-01-08 11:06:59 -0800

    Got the first festure done.

--- app/src/main/java/com/example/health_tracker/MainActivity.java
+++ app/src/main/java/com/example/health_tracker/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.health_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView SmashCounter;
+    public int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SmashCounter = findViewById(R.id.counter);
+    }
+
+    public void addToCounterOnClick(View v){
+        counter ++;
+        SmashCounter.setText(Integer.toString(counter));
     }
 }
